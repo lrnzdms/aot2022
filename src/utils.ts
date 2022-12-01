5bbc5f5a3f41bec1d8033453f180c69973142fa9
lrnzdms <lorenz.dames@gmail.com>
2022-12-01 10:09:11 +0100

    .

---
+++ src/utils.ts
@@ -0,0 +1,44 @@
+const fs = require("fs");
+const lineReader = require("readline");
+
+export const GetLinesAsNumbers = (path:string) => new Promise((res, rej) => {
+  const reader = lineReader.createInterface({
+      input: fs.createReadStream(path)
+  });
+  
+  const input:number[] = [];
+
+  reader.on("line", (l:string) => {
+      input.push(Number.parseInt(l));
+  })
+  reader.on("close", () => {
+      res(input);
+  })  
+})
+
+export const GetLines = (path: string): Promise<string[]> => new Promise((res, rej) => {
+  const reader = lineReader.createInterface({
+    input: fs.createReadStream(path)
+  });
+  const input: string[] = [];
+  reader.on("line", (l: string) => {
+    input.push(l);
+  })
+  reader.on("close", () => {
+    res(input);
+  })
+})
+
+export const GetNumbers = (path: string): Promise<number[]> => new Promise((res, rej) => {
+  const reader = lineReader.createInterface({
+    input: fs.createReadStream(path)
+  });
+  const input: number[] = [];
+  reader.on("line", (l: string) => {
+    const numbers = l.split(",").map(s => Number.parseInt(s))
+    input.push(...numbers);
+  })
+  reader.on("close", () => {
+    res(input);
+  })
+})
